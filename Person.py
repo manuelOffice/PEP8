3eb81d1559988c1aed8f97793440ed0a275698b3
manuelOffice <office.manuel.ab@gmail.com>
2024-08-23 20:58:20 -0400

    Ready to deploy
    Create class Person with getter and setter methods
    Adding greet(), full_name() and __str__() methods for Person
    Add testing lines of code in main funciton
    Change add_numbers method to receive more arguments
    Add documentation to class Timer and a block of code to be timed
    Fix .gitignore __pycache__

--- Person.py
+++ Person.py
@@ -1,41 +1,110 @@
 """
-Title:PEP 8 Compliance
+Title: Clase Persona
+Autor: José Manuel Alvarado Balderrama
+Date: 2024-08-20
+Version: v1.0
 """
 
-def add_numbers(a, b):
+class Person:
     """
-    Add two numbers together.
-
-    Parameters:
-        a (int or float): The first number to be added.
-        b (int or float): The second number to be added.
-
-    Returns:
-        int or float: The sum of the two input values.
+    This class provides the most common attributes and methods of a person.   
     """
-    result = a + b
-    return result
+    
+    def __init__(self, fname: str, lname: str) -> None:
+        """ 
+        This method creates an instance of a Person [object].
 
-def greet(name):
-    """Greets the user with a personalized message.
+        Args:
+            fname: 'first name' of type string
+            lname: 'last name' of type string
 
-    Args:
-        name: The name of the user to greet.
+        Returns:
+            There's not a return value
+        """
+        self.__age = None
+        self.__fname = fname
+        self.__lname = lname
 
-    Returns:
-        A string containing the greeting message.
-    """
+    # Getter and Setter Methods
 
-    if name:
-        return f"Hello, {name}!"
-    else:
-        return "Hello, world!"
+    @property
+    def age(self) -> int:
+        """ This method returns an integer value"""
+        return self.__age
+
+    @age.setter
+    def age(self, age: int) -> None:
+        """ This method recibes an integer value"""
+        self.__age = age
+
+    @property
+    def fname(self) -> str:
+        """ This method returns a string value"""
+        return self.__fname
+    
+    @property
+    def lname(self) -> str:
+        """ This method returns a string value"""
+        return self.__lname
+    
+    # Person class Methods
+    
+    def __str__(self) -> str:
+        """
+        This method makes the instantiated object printable.
+
+        Returns:
+            The most important attributes as a string.
+        """
+        full_name = "Full Name"
+        age = "Age"
+        return f"{full_name:<10}: {self.full_name()} \n{age:<10}: {self.age}"
+    
+    def full_name(self) -> str:
+        """ This method returns a string value"""
+        full_name = self.fname + " " + self.lname
+        return full_name
+
+    def greet(self, name: str = None) -> str:
+        """Greets the user with a personalized message.
+
+        Args:
+            name: The name of the user to greet.
+
+        Returns:
+            A string containing the greeting message.
+        """
+
+        if name:
+            return f"Hello, {name}. Nice to meet you!"
+        else:
+            return f"Hi, I'm {self.fname}!"
+        
+    def add_numbers(self, *args: int | float) -> int | float:
+        """
+        Add numbers together.
+
+        Parameters:
+            args (int or float): The numbers (in a tuple) to be added.
+
+        Returns:
+            int or float: The sum of the values.
+        """
+        result = 0
+        for number in (args):
+            result += number
+
+        return result
 
 
 if __name__== "__main__":
+    # Creating an object of the class Person
+    myself = Person('Manuel', 'Alvarado')
+
     #Llamada al metodo add_numbers()
-    number = add_numbers(20,2)
-    print(number)
+    number = myself.add_numbers(20, 2, 5.2)
+    print(f"Result = {number}")
+
+    # Greeting using an object Person
     user_name = input("Enter your name: ")
-    greeting = greet(user_name)
-    print(greeting)
+    print(myself.greet(user_name))
--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-""" Liberia time forma de importar una liberia """
+"""
+Title: Estilo Estandar de Codificación PEP8
+Autor: José Manuel Alvarado Balderrama
+Date: 2024-08-20
+Version: v1.0
+"""
+
+# Libraries that have been used
+from Person import *
 import time
 
 class Timer:
-    #
+    """
+    This clase is a Context Manager that times how long 
+    it takes to execute a block of code.
+    """
     def __enter__(self):
+        """
+        This method saves the moment when it was initiated
+
+        Returns:
+            An instance of itself
+        """
         self.start_time = time.time()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        This method shows the time required to execute a block of code.
+
+        Args:
+            exc_type: 'exception type' of type exception
+            exc_val: 'exception value' of type exception
+            exc_tb: 'exception traceback' of type exception
+        """
         end_time = time.time()
         print(f"Time taken: {end_time - self.start_time} seconds")
 
+# Block of code
 with Timer():
-    # Code to be timed goes here
-    pass
+    # Creating an object of the class Person
+    myself = Person('Manuel', 'Alvarado')
+    myself.age = 20
+
+    # Greeting using an object Person
+    print(myself.greet())
+
+    # Showing some attributes of this object
+    print(myself)
+
+print("Done!")
